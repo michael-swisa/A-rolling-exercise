e5cfd95a1267d4eff6a8803b57e820f5718e282e
michaelswisa <159651049+michaelswisa@users.noreply.github.com>
2024-02-13 17:09:28 +0200

    Add files via upload

--- Part One.py
+++ Part One.py
@@ -8,22 +8,28 @@ PRICE_ORANGE_JUICE = 4
 PRICE_APPLE_JUICE = 8
 PRICE_SPRITE = 8
 PRICE_COLA = 9
+payment = 0
 product = int(input("Type a product number\n"
-                    "1. Water - 4₪\n"
-                    "2. Orange juice - 4₪\n"
-                    "3. Apple juice - 8₪\n"
-                    "4. Sprite - 8₪\n"
-                    "5. Cole - 9₪\n"))
-if product == 1:
+                    "1. Water\n"
+                    "2. Orange juice\n"
+                    "3. Apple juice\n"
+                    "4. Sprite\n"
+                    "5. Cole\n"))
+if product == WATER:
     print("insert", PRICE_WATER, "₪")
-elif product == 2:
+    payment = PRICE_WATER
+elif product == ORANGE_JUICE:
     print("insert", PRICE_ORANGE_JUICE, "₪")
-elif product == 3:
+    payment = PRICE_ORANGE_JUICE
+elif product == APPLE_JUICE:
     print("insert", PRICE_APPLE_JUICE, "₪")
-elif product == 4:
+    payment = PRICE_APPLE_JUICE
+elif product == SPRITE:
     print("insert", PRICE_SPRITE, "₪")
-elif product == 5:
+    payment = PRICE_SPRITE
+elif product == COLA:
     print("insert", PRICE_COLA, "₪")
+    payment = PRICE_COLA
 else:
     print("Your choice is wrong")
 if 0 < product < 6:
@@ -32,58 +38,13 @@ if 0 < product < 6:
     coins_5 = int(input("Type how many 5₪ coins you insert\n"))
     coins_10 = int(input("Type how many 10₪ coins you insert\n"))
     amount_entered = coins_1 + coins_2 * 2 + coins_5 * 5 + coins_10 * 10
-    if product == 1:
-        if amount_entered >= PRICE_WATER:
-            print("Thank you for shopping with us")
-            print("The excess is", (amount_entered - PRICE_WATER),
-                  "\n(in coins 10)", (amount_entered - PRICE_WATER) // 10,
-                  "\n(in coins 5)", (amount_entered - PRICE_WATER) % 10 // 5,
-                  "\n(in coins 2)", (amount_entered - PRICE_WATER) % 10 % 5 // 2,
-                  "\n(in coins 1)", (amount_entered - PRICE_WATER) % 10 % 5 % 2)
-        else:
-            print("You did not insert enough coins and your money is returned\n"
-                  "You can log in again from the beginning\nYour surplus", amount_entered, "₪")
-    if product == 2:
-        if amount_entered >= PRICE_ORANGE_JUICE:
-            print("Thank you for shopping with us")
-            print("The excess is", (amount_entered - PRICE_ORANGE_JUICE),
-                  "\n(in coins 10)", (amount_entered - PRICE_ORANGE_JUICE) // 10,
-                  "\n(in coins 5)", (amount_entered - PRICE_ORANGE_JUICE) % 10 // 5,
-                  "\n(in coins 2)", (amount_entered - PRICE_ORANGE_JUICE) % 10 % 5 // 2,
-                  "\n(in coins 1)", (amount_entered - PRICE_ORANGE_JUICE) % 10 % 5 % 2)
-        else:
-            print("You did not insert enough coins and your money is returned\n"
-                  "You can log in again from the beginning\nYour surplus", amount_entered, "₪")
-    if product == 3:
-        if amount_entered >= PRICE_APPLE_JUICE:
-            print("Thank you for shopping with us")
-            print("The excess is", (amount_entered - PRICE_APPLE_JUICE),
-                  "\n(in coins 10)", (amount_entered - PRICE_APPLE_JUICE) // 10,
-                  "\n(in coins 5)", (amount_entered - PRICE_APPLE_JUICE) % 10 // 5,
-                  "\n(in coins 2)", (amount_entered - PRICE_APPLE_JUICE) % 10 % 5 // 2,
-                  "\n(in coins 1)", (amount_entered - PRICE_APPLE_JUICE) % 10 % 5 % 2)
-        else:
-            print("You did not insert enough coins and your money is returned\n"
-                  "You can log in again from the beginning\nYour surplus", amount_entered, "₪")
-    if product == 4:
-        if amount_entered >= PRICE_SPRITE:
-            print("Thank you for shopping with us")
-            print("The excess is", (amount_entered - PRICE_SPRITE),
-                  "\n(in coins 10)", (amount_entered - PRICE_SPRITE) // 10,
-                  "\n(in coins 5)", (amount_entered - PRICE_SPRITE) % 10 // 5,
-                  "\n(in coins 2)", (amount_entered - PRICE_SPRITE) % 10 % 5 // 2,
-                  "\n(in coins 1)", (amount_entered - PRICE_SPRITE) % 10 % 5 % 2)
-        else:
-            print("You did not insert enough coins and your money is returned\n"
-                  "You can log in again from the beginning\nYour surplus", amount_entered, "₪")
-    if product == 5:
-        if amount_entered >= PRICE_COLA:
-            print("Thank you for shopping with us")
-            print("The excess is", (amount_entered - PRICE_COLA),
-                  "\n(in coins 10)", (amount_entered - PRICE_COLA) // 10,
-                  "\n(in coins 5)", (amount_entered - PRICE_COLA) % 10 // 5,
-                  "\n(in coins 2)", (amount_entered - PRICE_COLA) % 10 % 5 // 2,
-                  "\n(in coins 1)", (amount_entered - PRICE_COLA) % 10 % 5 % 2)
-        else:
-            print("You did not insert enough coins and your money is returned\n"
+    if amount_entered >= payment:
+        print("Thank you for shopping with us")
+        print("The excess is", (amount_entered - payment),
+                  "\n(in coins 10)", (amount_entered - payment) // 10,
+                  "\n(in coins 5)", (amount_entered - payment) % 10 // 5,
+                  "\n(in coins 2)", (amount_entered - payment) % 10 % 5 // 2,
+                  "\n(in coins 1)", (amount_entered - payment) % 10 % 5 % 2)
+    else:
+        print("You did not insert enough coins and your money is returned\n"
                   "You can log in again from the beginning\nYour surplus", amount_entered, "₪")
